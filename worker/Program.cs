72a8b5fe50989cf2c45872e7e56425b9ac8a63c3
AlexsandroCampos <alexsandro.campos@usp.br>
2026-07-10 15:50:36 -0300

    do not remember

--- worker/Program.cs
+++ worker/Program.cs
@@ -9,15 +9,16 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.Configure<WorkerOptions>(builder.Configuration.GetSection("Worker"));
 
-builder.Services.AddSingleton<IEventInboxReader, PostgresEventInboxReader>();
 builder.Services.AddSingleton<IReadModelStore, RedisReadModelStore>();
 builder.Services.AddSingleton<IProjectionQueryRepository, PostgresProjectionQueryRepository>();
 
 builder.Services.AddSingleton<IEventProjectionHandler, GoalScoredProjectionHandler>();
 builder.Services.AddSingleton<IEventProjectionHandler, MatchEndedProjectionHandler>();
+builder.Services.AddSingleton<IEventProjectionHandler, FoulCommittedProjectionHandler>();
 builder.Services.AddSingleton<IEventRouter, EventRouter>();
 
 builder.Services.AddHostedService<WorkerService>();
+builder.Services.AddTransient<StandingsBuilderService>();
 
 var host = builder.Build();
 await host.RunAsync();
